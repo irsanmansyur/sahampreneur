02357bc76cbf454177fe9a58dc20aed963d28852
tantangin <irsan00mansyur@gmail.com>
2020-11-30 00:14:03 +0800

    revisi new

--- themes/public/rapid/components/section/video-saham.php
+++ themes/public/rapid/components/section/video-saham.php
@@ -8,14 +8,11 @@
         <h3>Video</h3>
         <!-- <p>Beberapa kategori video menarik yang bisa anda dapatkan ketika menjadi anggota premiun di situs kami..!</p> -->
       </header>
-      <div class="row">
+      <div class="row justify-content-center">
         <?php
         $background_colors = array('#00A4CCFF', '#F95700FF', '#ADEFD1FF', '#00203FFF', '#D6ED17FF', "#ED2B33FF", "#97BC62FF", "#2C5F2D", "#00539CFF");
-        if (count($kategories) == 4 || count($kategories) > 4) $count = 4;
-        else if (count($kategories) == 2 || count($kategories) == 4) $count = 6;
-        else $count = 12;
         foreach ($kategories as $kategori) : ?>
-          <div class="col-md-6 col-lg-<?= $count; ?> wow bounceInUp" data-wow-duration="1.4s">
+          <div class="col-md-6 col-lg-4  wow bounceInUp" data-wow-duration="1.4s">
             <div class="box">
               <img src="<?= $kategori->takeImage(); ?>" style="margin: 0 auto 15px auto;padding: 12px;display: inline-block;text-align: center;border-radius: 50%;width: 90px;height: 90px;background-color:<?= $background_colors[array_rand($background_colors)]; ?>">
               <h4 class="title"><a href=""><?= $kategori->name; ?></a></h4>
